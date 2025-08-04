aedcdbfe540aa38540ad029bcea2ce03f23e1e0e
RawDiamondMC <RawDiamondMC@outlook.com>
2025-08-04 13:45:26 +0800

    Output useful message

--- main.ts
+++ main.ts
@@ -1,22 +1,21 @@
 import * as core from '@actions/core'
 import * as http from '@actions/http-client'
 
-(async () => {
-    const auth = core.getInput("auth");
-    const endpoint = core.getInput("endpoint");
-    const run_id = core.getInput("run-id");
-    if (URL.parse(endpoint) == null) {
-        throw new Error('Invalid endpoint provided');
-    }
-    const httpClient = new http.HttpClient('kessoku-private-ci');
-    const headers = {
-        'Authorization': `Basic ${auth}`,
-        'User-Agent': 'kessoku-private-ci'
-    };
-    const response: http.HttpClientResponse = await httpClient.post(endpoint, JSON.stringify({run_id: run_id}), headers);
+const auth = core.getInput("auth");
+const endpoint = core.getInput("endpoint");
+const run_id = core.getInput("run-id");
+if (URL.parse(endpoint) == null) {
+    throw new Error('Invalid endpoint provided');
+}
+const httpClient = new http.HttpClient('kessoku-private-ci');
+const headers = {
+    'Authorization': `Basic ${auth}`,
+    'User-Agent': 'kessoku-private-ci'
+};
+httpClient.post(endpoint, JSON.stringify({run_id: run_id}), headers).then(response => {
     if (response.message.statusCode == 200) {
         core.info("Successfully post the API!");
     } else {
-        core.setFailed("Action failed!");
+        core.setFailed(`Action failed! Response status: ${response.message.statusCode}`);
     }
-})();
+});
